91617ceb0f4fbd2675e2a34cc95ee918c5162313
wuchengxing <1449697757@qq.com>
2021-12-14 11:30:00 +0800

    feat：简单的初始化一个netty案例

---
+++ netty-01-01-initnetty-demo/src/main/java/com/simple/server/MyChannelInitializer.java
@@ -0,0 +1,22 @@
+package com.simple.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-12-14 10:45
+ **/
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        System.out.println("链接报告开始");
+        System.out.println("链接报告信息：有一客户端链接到本服务端");
+        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + channel.localAddress().getPort());
+        System.out.println("链接报告完毕");
+    }
+}
