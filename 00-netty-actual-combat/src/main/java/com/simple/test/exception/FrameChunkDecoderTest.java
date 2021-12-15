143691bbebbf3c20df84727d61ffba0c076c0444
wuchengxing <1449697757@qq.com>
2021-12-15 16:15:35 +0800

    feat：netty实战-test-异常数据测试

---
+++ 00-netty-actual-combat/src/main/java/com/simple/test/exception/FrameChunkDecoderTest.java
@@ -0,0 +1,48 @@
+package com.simple.test.exception;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.TooLongFrameException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-12-15 16:14
+ **/
+public class FrameChunkDecoderTest {
+
+    @Test
+    public void testFramesDecoded() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+        ByteBuf input = buf.duplicate();
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new FrameChunkDecoder(3));
+        assertTrue(channel.writeInbound(input.readBytes(2)));
+        try {
+            channel.writeInbound(input.readBytes(4));
+            Assert.fail();
+        } catch (TooLongFrameException e) {
+            // expected exception
+        }
+        assertTrue(channel.writeInbound(input.readBytes(3)));
+        assertTrue(channel.finish());
+        // Read frames
+        ByteBuf read = (ByteBuf) channel.readInbound();
+        assertEquals(buf.readSlice(2), read);
+        read.release();
+        read = (ByteBuf) channel.readInbound();
+        assertEquals(buf.skipBytes(4).readSlice(3), read);
+        read.release();
+        buf.release();
+    }
+}
