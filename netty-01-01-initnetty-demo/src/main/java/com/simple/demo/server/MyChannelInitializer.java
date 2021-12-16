de7f1df83322af3fdaf8f713a2b6efeefd26c503
wuchengxing <1449697757@qq.com>
2021-12-16 13:52:45 +0800

    feat：netty实战-等待客户端连接

--- netty-01-01-initnetty-demo/src/main/java/com/simple/demo/server/MyChannelInitializer.java
+++ netty-01-01-initnetty-demo/src/main/java/com/simple/demo/server/MyChannelInitializer.java
@@ -15,8 +15,8 @@ public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel channel) throws Exception {
         System.out.println("链接报告开始");
         System.out.println("链接报告信息：有一客户端链接到本服务端");
-        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
-        System.out.println("链接报告Port:" + channel.localAddress().getPort());
+        System.out.println("客户端地址:" + channel.remoteAddress().getHostString());
+        System.out.println("客户端端口:" + channel.remoteAddress().getPort());
         System.out.println("链接报告完毕");
     }
 }
