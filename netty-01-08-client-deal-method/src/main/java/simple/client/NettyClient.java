76dac50348bb3762ddf4a251042a001dfafd8ce1
wuchengxing <1449697757@qq.com>
2021-12-16 18:07:57 +0800

    feat：netty实战-客户端的数据处理

---
+++ netty-01-08-client-deal-method/src/main/java/simple/client/NettyClient.java
@@ -0,0 +1,40 @@
+package simple.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import simple.init.MyChannelInitializer;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-12-16 17:42
+ **/
+public class NettyClient {
+
+    public static void main(String[] args) {
+        new NettyClient().connect("192.168.31.19", 7397);
+    }
+
+    private void connect(String inetHost, int inetPort) {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.AUTO_READ, true);
+            b.handler(new MyChannelInitializer());
+            ChannelFuture f = b.connect(inetHost, inetPort).sync();
+            System.out.println("客户端连接测试");
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ netty-01-08-client-deal-method/src/main/java/simple/handler/MyClientHandler.java
@@ -0,0 +1,59 @@
+package simple.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-12-16 18:02
+ **/
+public class MyClientHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当客户端主动链接服务端的链接后，这个通道就是活跃的了。也就是客户端与服务端建立了通信通道并且可以传输数据
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("链接报告开始");
+        System.out.println("链接报告信息：本客户端链接到服务端。channelId：" + channel.id());
+        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + channel.localAddress().getPort());
+        System.out.println("链接报告完毕");
+        //通知客户端链接建立成功
+        String str = "通知服务端链接建立成功" + " - " + channel.localAddress().getHostString() + "\r\n";
+        ctx.writeAndFlush(str);
+    }
+
+    /**
+     * 当客户端主动断开服务端的链接后，这个通道就是不活跃的。也就是说客户端与服务端的关闭了通信通道并且不可以传输数据
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("断开链接" + ctx.channel().localAddress().toString());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // 接收msg消息
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss - ").format(new Date()) + "接收到消息：" + msg);
+        //通知客户端链消息发送成功
+        String str = "客户端收到：" + new Date() + " " + msg + "\r\n";
+        ctx.writeAndFlush(str);
+    }
+
+    /**
+     * 抓住异常，当发生异常的时候，可以做一些相应的处理，比如打印日志、关闭链接
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+        System.out.println("异常信息：\r\n" + cause.getMessage());
+    }
+}
